649aadb00b89216ee85d27c823c81375aa722150
Blaine <blaine.d.oneill@gmail.com>
2021-02-28 06:59:04 -0800

    add recent feed

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -11,9 +11,9 @@
       <section class="search">
         SEARCH
       </section>
-      <div class="artists">
-        <div v-for="artist in artists" :key="artist._id">
-          <h2><a v-bind:href="artist.slug.current" v-text="artist.title" /></h2>
+      <div class="recent">
+        <div v-for="item in feed" :key="item._id" :class="item._type">
+          <nuxt-link :to="'/'+item._type+'s/'+item.slug.current" :class="item._type + '-title'" v-text="item.title" />
         </div>
       </div>
     </main>
@@ -27,12 +27,24 @@ import { groq } from '@nuxtjs/sanity'
 
 export default Vue.extend({
   async asyncData({ app: { $sanity }}) {
-    const query = groq`*[_type == "artist"]`
-    const artists = await $sanity.fetch(query)
-    return { artists }
+    const query = groq`*[_type in ["artist", "exhibition", "fair"]]`
+    const feed = await $sanity.fetch(query)
+    return { feed }
   }
 })
 </script>
 
-<style>
+<style lang="scss">
+main.index {
+  .recent {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .fair {
+    align-items: center;
+    align-self: flex-end;
+    justify-content: center;
+    padding: 1.5em;
+  }
+}
 </style>
