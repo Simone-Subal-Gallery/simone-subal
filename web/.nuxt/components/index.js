2d0e6ac3b38929bf720c366ebccb5f739a86b93e
Blaine <blaine.d.oneill@gmail.com>
2021-02-28 05:18:31 -0800

    remove node_modules/ and .nuxt/

--- web/.nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as SanityContent } from '../../node_modules/@nuxtjs/sanity/dist/components/sanity-content.js'
-export { default as SanityImage } from '../../node_modules/@nuxtjs/sanity/dist/components/sanity-image.js'
-export { default as Logo } from '../../components/Logo.vue'
-export { default as Nav } from '../../components/Nav.vue'
-
-export const LazySanityContent = import('../../node_modules/@nuxtjs/sanity/dist/components/sanity-content.js' /* webpackChunkName: "components/sanity-content" */).then(c => c.default || c)
-export const LazySanityImage = import('../../node_modules/@nuxtjs/sanity/dist/components/sanity-image.js' /* webpackChunkName: "components/sanity-image" */).then(c => c.default || c)
-export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
-export const LazyNav = import('../../components/Nav.vue' /* webpackChunkName: "components/nav" */).then(c => c.default || c)
