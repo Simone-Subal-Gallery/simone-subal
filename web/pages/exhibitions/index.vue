66b0d9cc1e94b9b92948c901932662a4651028f5
neener <janinemrosen@gmail.com>
2023-07-11 12:22:06 -0600

    add role tag to search divs

--- web/pages/exhibitions/index.vue
+++ web/pages/exhibitions/index.vue
@@ -51,7 +51,7 @@
           <a :class="view == 'grid'?'active':''" @click="setView('grid')">Grid</a>
           <a :class="view == 'list'?'active':''" @click="setView('list')">List</a>
         </div>
-        <div class="exhibition-search">
+        <div class="exhibition-search" role="search">
           <label id="search-exhibition-label" for="search-exhibition-input">
             <input id="search-exhibition-input" type="text" placeholder="Search" v-model="searchFeedValue" />
           </label>
